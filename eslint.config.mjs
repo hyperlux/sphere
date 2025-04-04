fe194564877ab98d081fc4f31a3deb94b6605a42
lightvibrations <polletkiro@gmail.com>
2025-04-04 14:38:37 +0000

    updated lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,18 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import next from '@next/eslint-plugin-next';
+import globals from 'globals';
 
 export default tseslint.config(
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
